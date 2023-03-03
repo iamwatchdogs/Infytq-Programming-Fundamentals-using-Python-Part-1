425b5c9d63b704ea5babe00470ebb96b6d87c086
Shamith <shamith301102@gmail.com>
2023-03-04 00:45:10 +0530

    Create Exercise on String - Level 2.py

---
+++ String in Python/Exercise on String - Level 2.py
@@ -0,0 +1,15 @@
+#lex_auth_012693763253788672132
+
+def generate_ticket(airline,source,destination,no_of_passengers):
+    ticket_number_list=[]
+    #Write your logic here
+    for i in range(no_of_passengers):
+        ticket_number_list.append(airline+':'+source[:3]+':'+destination+':'+str(101+i))
+        if len(ticket_number_list) > 5:
+            ticket_number_list.pop(0)
+
+    #Use the below return statement wherever applicable
+    return ticket_number_list
+
+#Provide different values for airline,source,destination,no_of_passengers and test your program
+print(generate_ticket("AI","Bangalore","London",7))
