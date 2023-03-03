865128e19892ce2bc0b4c116e6760887d3502bb0
Shamith <shamith301102@gmail.com>
2023-03-04 01:19:39 +0530

    Update Assignment on List - Level 1.py

--- Assignment Set - 3/Assignment on List - Level 1.py
+++ Assignment Set - 3/Assignment on List - Level 1.py
@@ -5,9 +5,9 @@ def find_leap_years(given_year):
     # Write your logic here
     list_of_leap_years = list()
     while len(list_of_leap_years) < 15:
-        given_year += 1
-        if (given_year%4==0 and given_year%100 != 0) or (given_year%400==0):
+        if (given_year%400==0 or (given_year%4==0 and given_year%100!=0)):
             list_of_leap_years.append(given_year)
+        given_year += 1
 
     return list_of_leap_years
 
