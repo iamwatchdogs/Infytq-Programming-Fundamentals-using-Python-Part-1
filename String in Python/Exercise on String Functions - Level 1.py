68e9c23d2f08564913b135c133053801982a5812
Shamith <shamith301102@gmail.com>
2023-03-04 00:51:37 +0530

    Create Exercise on String Functions - Level 1.py

---
+++ String in Python/Exercise on String Functions - Level 1.py
@@ -0,0 +1,24 @@
+#lex_auth_01269438070259712046
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    
+    #start writing your code here
+    #Populate the variables: count1 and count2
+    for i in name_list:
+        if len(i) == 3 and i.endswith('at'):
+            count1+=1
+        if 'at' in i:
+            count2+=1
+    
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+
+
+#Provide different names in the list and test your program
+name_list=["Hat","Cat","rabbit","matter"]
+count_names(name_list)
