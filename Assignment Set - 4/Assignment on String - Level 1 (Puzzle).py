299ed5e2a69659667acd1c618867eeeb1d56f9b1
Shamith <shamith301102@gmail.com>
2023-03-04 14:22:20 +0530

    Changed to multiline comment

--- Assignment Set - 4/Assignment on String - Level 1 (Puzzle).py
+++ Assignment Set - 4/Assignment on String - Level 1 (Puzzle).py
@@ -1,17 +1,18 @@
-# Problem Statement
-# Write a python program to display all the common characters between two strings. Return -1 if there are no matching characters.
-#
-# Note: Ignore blank spaces if there are any. Perform case sensitive string comparison wherever necessary.
+'''
+Problem Statement
+Write a python program to display all the common characters between two strings. Return -1 if there are no matching characters.
 
-# Sample Input
-#
-# "I like Python"
-# "Java is a very popular language"
-#
-# Expected output
-#
-# lieyo
+Note: Ignore blank spaces if there are any. Perform case sensitive string comparison wherever necessary.
 
+Sample Input
+
+"I like Python"
+"Java is a very popular language"
+
+Expected output
+
+lieyo
+'''
 
 #lex_auth_012693825794351104168
 
