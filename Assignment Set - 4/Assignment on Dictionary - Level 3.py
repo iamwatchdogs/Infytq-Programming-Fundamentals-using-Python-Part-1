5fd555b7d07842daa0a2c527ad766586aaeb59ee
Shamith <shamith301102@gmail.com>
2023-03-04 14:24:33 +0530

    More readable question

--- Assignment Set - 4/Assignment on Dictionary - Level 3.py
+++ Assignment Set - 4/Assignment on Dictionary - Level 3.py
@@ -1,6 +1,8 @@
 '''
 Problem Statement
-Care hospital wants to know the medical speciality visited by the maximum number of patients. Assume that the patient id of the patient along with the medical speciality visited by the patient is stored in a list. The details of the medical specialities are stored in a dictionary as follows:
+Care hospital wants to know the medical speciality visited by the maximum number of patients. 
+Assume that the patient id of the patient along with the medical speciality visited by the patient is stored in a list.
+The details of the medical specialities are stored in a dictionary as follows:
 {
 "P":"Pediatrics",
 "O":"Orthopedics",
